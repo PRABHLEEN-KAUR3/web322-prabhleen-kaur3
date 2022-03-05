42e14dafc815cd4f138f95966ad38e26f8f73070
Prabhleen Kaur <prabhleen-kaur3@myseneca.ca>
2022-03-04 23:10:44 -0500

    Removing regular expression validation for login form

--- controllers/general.js
+++ controllers/general.js
@@ -1,4 +1,14 @@
-
+/************************************************************************************
+* WEB322 â€“ Project (Winter 2022)
+* I declare that this assignment is my own work in accordance with Seneca Academic
+* Policy. No part of this assignment has been copied manually or electronically from
+* any other source (including web sites) or distributed to other students.
+*
+* Name: Prabhleen Kaur
+* Student ID: 146094206
+* Course/Section: WEB322/NDD
+*
+************************************************************************************/
 const express = require('express');
 const router = express.Router();
 const app = express();
@@ -39,9 +49,7 @@ router.post("/Login", (req, res) => {
   let passedValidation = true;
   let validationMessages = {};
 
-    //Regular expression for email from: https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
-    var form = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if(!email.match(form) || email.trim().length === 0){
+  if(email.trim().length === 0){
     passedValidation = false;
     validationMessages.email = "*** Required Field *** Please fill in your email address."
   }
